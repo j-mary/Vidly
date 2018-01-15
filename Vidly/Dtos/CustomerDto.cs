7d7a7039b827fc9504b68670ff372723e9bc4682
Jude Ocheje <ochejejude@gmail.com>
2018-01-15 10:18:36 +0100

    Builth Api & Added jQuery DataTable

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipTypeDto MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+    }
+}
